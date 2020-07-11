8ae75a5b781a4fa19eff9470ab527796fb80346b
Charly Escalona <charly2221@hotmail.fr>
2020-07-11 18:12:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 * Projet créé par Charly Escalona
 * Charly2221@hotmail.fr
 * www.charly-e.com
+* https://github.com/L4znet/menu_burger_rotate
 */
 $(document).ready(function(){
     let menu = $('nav');
